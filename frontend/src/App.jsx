62bd3be29168b7de3aa4aefea0fc2dfb03d17eb7
gursam <gursam.madan@gmail.com>
2025-06-11 01:12:56 +0530

    the Project is complete

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,7 +9,8 @@ import Publish from "./pages/Publish";
 const App = () => {
   return (
     <Routes>
-      <Route path={["/", "/signup"]} element={<Signup />} />
+      <Route path="/" element={<Signup />} />
+      <Route path="/signup" element={<Signup />} />
       <Route path="/signin" element={<Signin />} />
       <Route path="/blog/:id" element={<Blog />} />
       <Route path="/blogs" element={<Blogs />} />
